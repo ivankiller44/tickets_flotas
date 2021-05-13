075af9b0cd0389a97ed44a78e1c7d3360238a59a
JoseViscarra <jeviscarra@gmail.com>
2021-05-12 21:54:56 -0400

    Route Path
    Creacion de routing path de las paginas

--- angular-tickets-flotas/src/app/app-routing.module.ts
+++ angular-tickets-flotas/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BusComponent } from './bus/bus.component';
+import { DateTimePriceComponent } from './date-time-price/date-time-price.component';
+import { RoutesComponent } from './routes/routes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'routes', component:RoutesComponent},
+  {path: 'date-time-price', component:DateTimePriceComponent},
+  {path: 'bus', component:BusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
