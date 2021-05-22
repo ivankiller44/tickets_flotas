47b51438d07e1c948e86dde83b023a893a118356
ivankiller44 <ivan.ga.ba44@gmail.com>
2021-05-22 00:22:24 -0400

    cambios en el api del serializador

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -11,7 +11,7 @@ def user_api_view(request):
     #list
     if request.method == 'GET':
         #queryset
-        users = User.objects.all()
+        users = User.objects.all().values('id', 'username', 'email', 'password')
         users_serializer = UserSerializer(users, many = True)
         return Response(users_serializer.data, status = status.HTTP_200_OK)
     
