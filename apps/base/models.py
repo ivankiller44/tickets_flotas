be1e4c2552fdd1943c17003d2c85ef4e54469ba1
ivankiller44 <ivan.ga.ba44@gmail.com>
2021-05-22 23:19:52 -0400

    nuevos modelos

--- apps/base/models.py
+++ apps/base/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 # Create your models here.
 class BaseModel(models.Model):
+    """Model definition for BaseModel."""
 
-    
+    # TODO: Define fields here
+    id = models.AutoField(primary_key = True)
+    state = models.BooleanField('Estado',default = True)
+    created_date = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+    modified_date = models.DateField('Fecha de Modificación', auto_now=True, auto_now_add=False)
+    deleted_date = models.DateField('Fecha de Eliminación', auto_now=True, auto_now_add=False)
 
     class Meta:
-        verbose_name = _("BaseModel")
-        verbose_name_plural = _("BaseModels")
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("BaseModel_detail", kwargs={"pk": self.pk})
+        """Meta definition for BaseModel."""
+        abstract = True
+        verbose_name = 'Modelo Base'
+        verbose_name_plural = 'Modelos Base'
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
+from apps.base.models import BaseModel
 # Create your models here.
