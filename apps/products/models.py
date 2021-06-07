d298420d2d6843e8c64de5226bbea35fcb181ba2
ivankiller44 <ivan.ga.ba44@gmail.com>
2021-06-07 15:54:27 -0400

    modificacion de las clases

--- apps/products/models.py
+++ apps/products/models.py
@@ -29,7 +29,6 @@ class MeasureUnit(BaseModel):
 class CategoryProduct(BaseModel):
     
     description = models.CharField('Description', max_length=50, unique=True, null=False, blank=False)
-    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE, verbose_name= 'Unidad de Medida')
     historical = HistoricalRecords()
 
     @property
@@ -75,6 +74,8 @@ class Product(BaseModel):
     name = models.CharField('Nombre de Producto', max_length=150, unique=True, blank=False, null=False)
     description = models.TextField('Descripcion de Producto', blank=False, null=False)
     image = models.ImageField('Imagen del Producto', upload_to='products/', blank=True, null=True)
+    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE, verbose_name='Unidad de medida', null=True)
+    category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name='Categoria de Producto', null=True)
     historical = HistoricalRecords()
 
     @property
