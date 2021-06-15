c559b28b1d6749c6e1ef381f1401875c4e0ec0a9
ivankiller44 <ivan.ga.ba44@gmail.com>
2021-06-14 23:35:03 -0400

    cambios en los modelos

--- apps/products/models.py
+++ apps/products/models.py
@@ -5,10 +5,10 @@ from apps.base.models import BaseModel
 # Create your models here.
 
 
-class MeasureUnit(BaseModel):
+class Departamento(BaseModel):
 
     # TODO> Define Fields Here
-    description = models.CharField('Description', max_length=50, blank=False, null=False, unique=True)
+    departamento = models.CharField('Description', max_length=50, blank=False, null=False, unique=True)
     historical = HistoricalRecords()
 
     @property
@@ -20,15 +20,15 @@ class MeasureUnit(BaseModel):
         self.changed_by = value
 
     class Meta:
-        verbose_name = ("Unidad de Medida")
-        verbose_name_plural = ("Unidades de Medidas")
+        verbose_name = ("Departamento")
+        verbose_name_plural = ("Departamentos")
 
     def __str__(self):
-        return self.description
+        return self.departamento
 
-class CategoryProduct(BaseModel):
+class TipoDeBus(BaseModel):
     
-    description = models.CharField('Description', max_length=50, unique=True, null=False, blank=False)
+    tipo = models.CharField('Description', max_length=50, unique=True, null=False, blank=False)
     historical = HistoricalRecords()
 
     @property
@@ -40,16 +40,15 @@ class CategoryProduct(BaseModel):
         self.changed_by = value
 
     class Meta:
-        verbose_name = ("Categoria de Producto")
-        verbose_name_plural = ("Categorias de Productos")
+        verbose_name = ("Tipo de Bus")
+        verbose_name_plural = ("Tipos de Buses")
 
     def __str__(self):
-        return self.description
+        return self.tipo
 
-class Indicator(BaseModel):
+class NumeroDeBus(BaseModel):
 
-    descount_value = models.PositiveIntegerField(default = 0)
-    category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name = 'Indicador de Oferta')
+    numero_de_bus = models.CharField('Numero De Bus', max_length=10, unique=True, null=False, blank=False)
     historical = HistoricalRecords()
 
     @property
@@ -61,22 +60,71 @@ class Indicator(BaseModel):
         self.changed_by = value
 
     class Meta:
-        verbose_name = ("Indicador de Oferta")
-        verbose_name_plural = ("Indicadores de Ofertas")
+        verbose_name = ("Numero de Bus")
+        verbose_name_plural = ("Numeros de Buses")
 
     def __str__(self):
-        return f'Oferta de la categoria {self.category_product} : {self.descount_value}%'
+        return self.numero_de_bus
 
+class FechaDeSalida(BaseModel):
 
-
-class Product(BaseModel):
-
-    name = models.CharField('Nombre de Producto', max_length=150, unique=True, blank=False, null=False)
-    description = models.TextField('Descripcion de Producto', blank=False, null=False)
-    image = models.ImageField('Imagen del Producto', upload_to='products/', blank=True, null=True)
-    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE, verbose_name='Unidad de medida', null=True)
-    category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name='Categoria de Producto', null=True)
+    fecha_de_salida = models.DateField('Fecha de Salida')
     historical = HistoricalRecords()
+
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
+
+    class Meta:
+        verbose_name = ("Fecha de Salida")
+        verbose_name_plural = ("Fechas de Salidas")
+
+    def __str__(self):
+        return self.fecha_de_salida
+
+class HoraDeSalida(BaseModel):
+
+    hora_de_salida = models.TimeField('Hora de Salida')
+    historical = HistoricalRecords()
+
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
+
+    class Meta:
+        verbose_name = ("Hora de Salida")
+        verbose_name_plural = ("Horas de Salidas")
+
+    def __str__(self):
+        return self.hora_de_salida
+
+
+class Viaje(BaseModel):
+
+    origen = models.ForeignKey(Departamento, on_delete=models.CASCADE, verbose_name='Departamento')
+    destino = models.ForeignKey(Departamento, on_delete=models.CASCADE, verbose_name='Departamento')
+
+    tipo_de_bus = models.ForeignKey(TipoDeBus, on_delete=models.CASCADE, verbose_name='Tipo de Bus')
+    numero_de_bus = models.ForeignKey(NumeroDeBus, on_delete=models.CASCADE, verbose_name='Numero de Bus')
+    fecha_de_salida = models.ForeignKey(FechaDeSalida, on_delete=models.CASCADE, verbose_name='Fecha de Salida')
+    hora_de_salida = models.ForeignKey(HoraDeSalida, on_delete=models.CASCADE, verbose_name='Hora de Salida')
+    estado = models.BooleanField('Estado del Bus', null=True)
+    precio = models.PositiveIntegerField()
+
+    #name = models.CharField('Nombre de Producto', max_length=150, unique=True, blank=False, null=False)
+    #description = models.TextField('Descripcion de Producto', blank=False, null=False)
+    #image = models.ImageField('Imagen del Producto', upload_to='products/', blank=True, null=True)
+    #measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE, verbose_name='Unidad de medida', null=True)
+    #category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name='Categoria de Producto', null=True)
+    #historical = HistoricalRecords()
 
     @property
     def _history_user(self):
